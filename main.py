9696421f464eec51eb18032fb17aa1d7c5e5601b
vss-2 <32624576+vss-2@users.noreply.github.com>
2019-06-12 15:04:37 -0300

    Ja ja acaba
    Tá quase funcionando

--- main.py
+++ main.py
@@ -2,23 +2,25 @@ lerArquivo = open('jantas.txt', 'r')
 
 try:
     entrada = input()
+    entrada = entrada.split(' ')
     
     # Entrada pode ser no formato:
     # adicionar X, sendo X inteiro
     # remover
+    valorAntigo = lerArquivo.read()
     
-    if (entrada[1] == 'adicionar'):
-        valorAntigo = lerArquivo.read()
+    if (entrada[0] == 'adicionar'):
         valorAntigo = int(valorAntigo.replace('\n',''))
-        valorNovo = int(entrada[2])
+        valorNovo = int(entrada[1])
         valorNovo = valorAntigo + valorNovo
-    else:
-        # Caso de remover
-        valorAntigo = lerArquivo.read()
+    else: 
+	#Caso de remover
         valorAntigo = int(valorAntigo.replace('\n',''))
         valorNovo = valorAntigo - 1
+
+    print('Temos agora, no total:', valorNovo)
     
-    escArquivo = write('jantas.txt', 'w+')
+    escArquivo = write('jantas.txt', 'w')
     escArquivo.write(valorNovo)
     escArquivo.close()
     
@@ -26,6 +28,8 @@ try:
         print('Atencao, esta e sua penultima janta')
     elif(valorNovo == 0):
         print('Atencao, esta foi sua ultima janta')
+        
+    exit()
 
 except NameError:
     pass
