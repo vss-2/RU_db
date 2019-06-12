afd0953b139507e0f99fd117978aa3b9bcdc44ac
vss-2 <32624576+vss-2@users.noreply.github.com>
2019-06-12 16:20:28 -0300

    Finalizado

--- main.py
+++ main.py
@@ -1,35 +1,44 @@
+# -*- coding: utf-8 -*-
+
 lerArquivo = open('jantas.txt', 'r')
 
 try:
-    entrada = input()
-    entrada = entrada.split(' ')
-    
-    # Entrada pode ser no formato:
-    # adicionar X, sendo X inteiro
-    # remover
-    valorAntigo = lerArquivo.read()
-    
-    if (entrada[0] == 'adicionar'):
-        valorAntigo = int(valorAntigo.replace('\n',''))
-        valorNovo = int(entrada[1])
-        valorNovo = valorAntigo + valorNovo
-    else: 
-	#Caso de remover
-        valorAntigo = int(valorAntigo.replace('\n',''))
-        valorNovo = valorAntigo - 1
+	try:
+		entrada = input()
+		entrada = entrada.split(' ')
+		
+		# Entrada pode ser no formato:
+		# adicionar X, sendo X inteiro
+		# remover
+		valorAntigo = lerArquivo.read()
+		
+		if (entrada[0] == 'adicionar'):
+			valorAntigo = int(valorAntigo.replace('\n',''))
+			valorNovo = int(entrada[1])
+			valorNovo = valorAntigo + valorNovo
+		else: 
+		#Caso de remover
+			valorAntigo = int(valorAntigo.replace('\n',''))
+			valorNovo = valorAntigo - 1
 
-    print('Temos agora, no total:', valorNovo)
-    
-    escArquivo = write('jantas.txt', 'w')
-    escArquivo.write(valorNovo)
-    escArquivo.close()
-    
-    if(valorNovo == 1):
-        print('Atencao, esta e sua penultima janta')
-    elif(valorNovo == 0):
-        print('Atencao, esta foi sua ultima janta')
-        
-    exit()
+		print('Temos agora, no total:', valorNovo)
+		
 
+		escArquivo = open('jantas.txt', 'w')
+
+		output = str(valorNovo) + " "
+		escArquivo.write(output)
+		escArquivo.close()
+		
+		if(valorNovo == 1):
+			print('Atenção, esta é sua penultima janta')
+		elif(valorNovo == 0):
+			print('Atenção, esta foi sua última janta')
+			
+		exit()
+		
+	except ValueError:
+		print('Valor inserido não é inteiro, parando execução')
+    
 except NameError:
     pass
